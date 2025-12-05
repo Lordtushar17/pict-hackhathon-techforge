8c25132c6e03b3f5e391ca13b4c7dd78ec21aab0
Tushar Chavan <tac8956@gmail.com>
2025-12-05 20:44:13 +0530

    addedd the folder for webapp

---
+++ babyguard/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return <div className="w-full h-screen bg-white">
+    <h1 className="text-3xl font-bold underline text-center mt-10">
+      Welcome to BabyGuard!
+    </h1>
+    <h2 className="text-xl text-center mt-5">
+      Your ultimate baby monitoring solution.
+    </h2>
+  </div>
+}
